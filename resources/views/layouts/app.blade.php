8fe45547980d5e78f38fc84761388a0506ef79d7
jessyrc <yessy2015rive@gmail.com>
2021-05-29 21:45:11 -0600

    Mostrar mensajes de exito y fails

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,6 +1,6 @@
 <!doctype html>
 <html lang="en">
- 
+
 <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
@@ -11,10 +11,12 @@
     <link rel="stylesheet" href="{!! asset('assets/libs/css/style.css')!!}">
     <link rel="stylesheet" href="{!! asset('assets/vendor/fonts/fontawesome/css/fontawesome-all.css')!!}">
 
-    <link rel="stylesheet" type="text/css" href="{!! asset('assets/vendor/datatables/css/dataTables.bootstrap4.css')!!}">
+    <link rel="stylesheet" type="text/css"
+        href="{!! asset('assets/vendor/datatables/css/dataTables.bootstrap4.css')!!}">
     <link rel="stylesheet" type="text/css" href="{!! asset('assets/vendor/datatables/css/buttons.bootstrap4.css')!!}">
     <link rel="stylesheet" type="text/css" href="{!! asset('assets/vendor/datatables/css/select.bootstrap4.css')!!}">
-    <link rel="stylesheet" type="text/css" href="{!! asset('assets/vendor/datatables/css/fixedHeader.bootstrap4.css')!!}">
+    <link rel="stylesheet" type="text/css"
+        href="{!! asset('assets/vendor/datatables/css/fixedHeader.bootstrap4.css')!!}">
     <title>RPac - El Salvador</title>
 </head>
 
@@ -29,20 +31,31 @@
         <div class="dashboard-header">
             <nav class="navbar navbar-expand-lg bg-white fixed-top">
                 <a class="navbar-brand" href="{{route('home')}}">R-Pac</a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <button class="navbar-toggler" type="button" data-toggle="collapse"
+                    data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                    aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse " id="navbarSupportedContent">
                     <ul class="navbar-nav ml-auto navbar-right-top">
                         <li class="nav-item dropdown connection">
-                            <a class="nav-link" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><p>{{Auth::user()->codigo_usuario}} - Usuario {{Auth::user()->obtenerRol()->nombre_rol}}</p></a>
+                            <a class="nav-link" id="navbarDropdown" role="button" data-toggle="dropdown"
+                                aria-haspopup="true" aria-expanded="false">
+                                <p>{{Auth::user()->codigo_usuario}} - Usuario {{Auth::user()->obtenerRol()->nombre_rol}}
+                                </p>
+                            </a>
                         </li>
                         <li class="nav-item dropdown nav-user">
-                            <a class="nav-link nav-user-img" href="#" id="navbarDropdownMenuLink2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><img src="{!! asset('assets/images/avatar-1.jpg')!!}" alt="" class="user-avatar-md rounded-circle"></a>
-                            <div class="dropdown-menu dropdown-menu-right nav-user-dropdown" aria-labelledby="navbarDropdownMenuLink2">
+                            <a class="nav-link nav-user-img" href="#" id="navbarDropdownMenuLink2"
+                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><img
+                                    src="{!! asset('assets/images/avatar-1.jpg')!!}" alt=""
+                                    class="user-avatar-md rounded-circle"></a>
+                            <div class="dropdown-menu dropdown-menu-right nav-user-dropdown"
+                                aria-labelledby="navbarDropdownMenuLink2">
                                 <div class="nav-user-info">
                                     <h5 class="mb-0 text-white nav-user-name">{{Auth::user()->codigo_usuario}}</h5>
-                                    <span class="status"></span><span class="ml-2">{{Auth::user()->obtenerRol()->nombre_rol}}</span>
+                                    <span class="status"></span><span
+                                        class="ml-2">{{Auth::user()->obtenerRol()->nombre_rol}}</span>
                                 </div>
                                 <a class="dropdown-item" href="{{ route('logout') }}"
                                     onclick="event.preventDefault();document.getElementById('logout-form').submit();">
@@ -70,28 +83,33 @@
             <div class="menu-list">
                 <nav class="navbar navbar-expand-lg navbar-light">
                     <a class="d-xl-none d-lg-none" href="#">Dashboard</a>
-                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+                        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
-                   
+
                     <div class="collapse navbar-collapse" id="navbarNav">
                         <ul class="navbar-nav flex-column">
                             <li class="nav-divider">
                                 Menu
-                            </li> 
-                            @if(Auth::user()->obtenerRol()->nombre_rol=="Táctico" || Auth::user()->obtenerRol()->nombre_rol=="Estratégico")
+                            </li>
+                            @if(Auth::user()->obtenerRol()->nombre_rol=="Táctico" ||
+                            Auth::user()->obtenerRol()->nombre_rol=="Estratégico")
                             <li class="nav-item">
-                                <a class="nav-link" href="{{route('tacticos')}}"><i class="fas fa-fw fa-file"></i>Reportes tácticos</a>
+                                <a class="nav-link" href="{{route('tacticos')}}"><i
+                                        class="fas fa-fw fa-file"></i>Reportes tácticos</a>
                             </li>
                             @endif
                             @if(Auth::user()->obtenerRol()->nombre_rol=="Estratégico")
                             <li class="nav-item">
-                                <a class="nav-link" href="{{route('estrategicos')}}"><i class="fas fa-fw fa-file"></i>Reportes estratégicos</a>
+                                <a class="nav-link" href="{{route('estrategicos')}}"><i
+                                        class="fas fa-fw fa-file"></i>Reportes estratégicos</a>
                             </li>
                             @endif
                             @if(Auth::user()->obtenerRol()->nombre_rol=="Administrador")
                             <li class="nav-item">
-                                <a class="nav-link" href="#"><i class="fas fa-fw fa-users"></i>Usuarios</a>
+                                <a class="nav-link" href="{{route('users-index')}}"><i
+                                        class="fas fa-fw fa-users"></i>Usuarios</a>
                             </li>
                             <li class="nav-item">
                                 <a class="nav-link" href="#"><i class="fas fa-fw fa-list"></i>Bitácora</a>
@@ -105,7 +123,8 @@
         <!-- ============================================================== -->
         <!-- end left sidebar -->
         <!-- ============================================================== -->
-    @yield('content')
+
+        @yield('content')
     </div>
     <!-- ============================================================== -->
     <!-- end main wrapper  -->
@@ -122,6 +141,22 @@
     <script src="{!! asset('assets/vendor/datatables/js/jquery.dataTables.min.js')!!}"></script>
     <script src="{!! asset('assets/vendor/datatables/js/dataTables.bootstrap4.min.js')!!}"></script>
     <script src="{!! asset('assets/vendor/datatables/js/data-table.js')!!}"></script>
+    <!-- jQuery -->
+    <script src="{{asset("/plugins/jquery/jquery.min.js")}}"></script>
+    <!-- jQuery UI 1.11.4 -->
+    <!-- Bootstrap 4 -->
+    <script src="{{asset("/plugins/bootstrap/js/bootstrap.bundle.min.js")}}"></script>
+    <!-- daterangepicker -->
+    <script src="{{asset("/plugins/moment/moment-with-locales.min.js")}}""></script>
+    <script>
+    $('.open-modal').click(function(e){
+         var $form = $(this).closest('form');
+         e.preventDefault();
+         $('#deleteModal').modal('show').on('click', '#delete', function(e) {
+             $form.trigger('submit');
+         });
+     });
+ </script>
 </body>
  
 </html>
